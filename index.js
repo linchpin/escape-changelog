9da96807231f5a899f1f15dd0fab1c645689c58b
Aaron Ware <aaron@linchpin.com>
2022-01-11 11:55:41 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    let changelog = core.getInput('changelog_entry');
+        changelog = changelog.replace(/\\n/g, "\\n")
+                              .replace(/\\'/g, "\\'")
+                              .replace(/\\"/g, '\\"')
+                              .replace(/\\&/g, "\\&")
+                              .replace(/\\r/g, "\\r")
+                              .replace(/\\t/g, "\\t")
+                              .replace(/\\b/g, "\\b")
+                              .replace(/\\f/g, "\\f");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    core.setOutput('escaped_changelog', changelog );
   } catch (error) {
     core.setFailed(error.message);
   }
